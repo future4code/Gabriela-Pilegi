d085e6548336ab18b181a937a689a0b3880ec2bf
unknown <email-que-voce-usa-no-github>
2020-12-02 16:35:15 -0300

    Exercícios de condicionais com desafios (2 não está finalizado)

--- semana3/condicionais/js-vanilla-template/src/index.js
+++ semana3/condicionais/js-vanilla-template/src/index.js
@@ -68,6 +68,8 @@
 //     console.log("Escolha outro filme! :(")
 // }
 
+
+//DESAFIO 1
 // const generoFilme = prompt("Qual gênero de filme você vai assistir?")
 
 // const precoIngresso = prompt("Qual o valor do ingresso do filme? (Digite apenas números)")
@@ -78,3 +80,64 @@
 //     console.log("Escolha outro filme! :(")
 // }
 
+//DESAFIO 2
+
+// const nome = prompt("Digite seu nome: ")
+// const tipoJogo = prompt("Digite o tipo de jogo (IN para internacional e DO para doméstico)")
+// const etapaJogo = prompt("Digite a etapa do jogo (SF indica semi-final; DT indica decisão de terceiro lugar; e FI indica final)")
+// const categoria = prompt("Digite a categoria (1, 2, 3 ou 4)")
+// const quantidade = Number(prompt("Digite a quantidade de ingressos desejada: "))
+// let valorIngresso = ""
+// let totalValor = " "
+
+// if(tipoJogo === "do") {
+//     if (etapaJogo === "sf" && categoria === 1) {
+//         valorIngresso === 1320
+//     } else if (etapaJogo === "sf" && categoria === 2) {
+//         valorIngresso === 880
+//     } else if (etapaJogo === "sf" && categoria === 3) {
+//         valorIngresso === 550
+//     } else if (etapaJogo === "sf" && categoria === 4) {
+//         valorIngresso === 220
+
+//     } else if (etapaJogo === "dt" && categoria === 1) {
+//         valorIngresso === 660
+//     } else if (etapaJogo === "dt" && categoria === 2) {
+//         valorIngresso === 440
+//     } else if (etapaJogo === "dt" && categoria === 3) {
+//         valorIngresso === 330
+//     } else if (etapaJogo === "dt" && categoria === 4) {
+//         valorIngresso === 170
+
+//     } else if (etapaJogo === "fi" && categoria === 1) {
+//             valorIngresso === 1980
+//     } else if (etapaJogo === "fi" && categoria === 2) {
+//         valorIngresso === 1320
+//     } else if (etapaJogo === "fi" && categoria === 3) {
+//         valorIngresso === 880
+//     } else if (etapaJogo === "fi" && categoria === 4) {
+//         valorIngresso === 330
+//     }
+    
+//     console.log("---Dados da compra---")
+//     console.log("Nome do cliente: ", nome)
+//     if(tipoJogo ==="do") {
+//         console.log("Tipo de jogo: Nacional")
+//     } else if(tipoJogo ==="in") {
+//         console.log("Tipo de jogo: Internacional")
+//     }
+//     if (etapaJogo ==="sf") {
+//         console.log("Etapa do jogo: Semifinal")
+//     } else if (etapaJogo ==="dr"){
+//         console.log("Etapa do jogo: Decisão do 3° lugar")
+//     } else if (etapaJogo ==="fi") {
+//         console.log("Etapa do jogo: Final")
+//     }
+
+//     console.log("Categoria: ", categoria)
+//     console.log("Quantidade de ingressos: ", quantidade)
+//     console.log("---Valores---")
+//     console.log("Valor do ingresso: R$", valorIngresso)
+//     //totalValor = (valorIngresso * 1 * quantidade)
+//     console.log("Valor total: R$", valorIngresso*quantidade)
+// }
