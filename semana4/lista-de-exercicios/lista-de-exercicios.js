a481f135bea8ed2c86b08b6c72047dd792087c4c
unknown <email-que-voce-usa-no-github>
2020-12-11 11:56:43 -0300

    Exercício 1 de interpretação de código.

---
+++ semana4/lista-de-exercicios/lista-de-exercicios.js
@@ -0,0 +1,6 @@
+// Exercícios de interpretação de código
+// Exercício 1
+// O que faz: Função para conversão de dólar em real após o usuário digitar o valor da cotação no dia.
+// Como faz: Cria uma função chamada conversorDeMoeda com o parâmetro valorEmDolar. Dentro da função, é definido um prompt a partir da variável cotacao para que o usuário informe o valor da cotação do dólar naquele momento. Em seguida, ainda dentro da função, é retornado o parâmetro valorEmDolar multiplicado pela cotação digitada pelo usuário. Após concluir a função, é definida a variável meuDinheiro, com a função tendo valor 100 como parâmetro e, por fim, é impresso no console o resultado obtido da multiplicação de 100 pela cotação
+// Valor impresso no console: Se o valor da cotação informado for R$ 5,07, será exibido R$ 507. 
+
