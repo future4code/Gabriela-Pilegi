2e99f72825362a106c29048283a00b9fad82e28a
unknown <email-que-voce-usa-no-github>
2020-12-11 11:57:08 -0300

    Exercício 2 de interpretação de código.

--- semana4/lista-de-exercicios/lista-de-exercicios.js
+++ semana4/lista-de-exercicios/lista-de-exercicios.js
@@ -4,3 +4,8 @@
 // Como faz: Cria uma função chamada conversorDeMoeda com o parâmetro valorEmDolar. Dentro da função, é definido um prompt a partir da variável cotacao para que o usuário informe o valor da cotação do dólar naquele momento. Em seguida, ainda dentro da função, é retornado o parâmetro valorEmDolar multiplicado pela cotação digitada pelo usuário. Após concluir a função, é definida a variável meuDinheiro, com a função tendo valor 100 como parâmetro e, por fim, é impresso no console o resultado obtido da multiplicação de 100 pela cotação
 // Valor impresso no console: Se o valor da cotação informado for R$ 5,07, será exibido R$ 507. 
 
+// Exercício 2
+// O que faz: Função para cálculo de investimento em diferentes categorias.
+// Como faz: É criada a função investeDinheiro com os parâmetros tipoDeInvestimento e valor. Dentro dessa função, é definida uma variável chamada valorAposInvestimento, seguida de uma condicional de switch/case para poupança, renda fixa, CDB e ações, além de um alerta para uma informação que diverge de um desses 4 casos. Dentro de cada case, há a multiplicação do valor pelo tipo de investimento específico. Fora do switch/case, mas ainda dentro da função, é retornada a variável valorAposInvestimento, para posterior impressão no console. Fora da função, é retornado o nome da função por meio das variáveis novoMontante e segundoMontante, que têm, respectivamente, o valor de 1.1 (correspondente ao case ações) multiplicado por 150 (correspondente ao parâmetro valor), e Tipo de investimento incorreto, uma vez que não há case para Tesouro Direto. Os resultados são impressos no console por meio do console.log.
+// Valor impresso no console: 165 para a variável novoMontante e Tipo de investimento informado incorreto para o segundoMontante.
+
