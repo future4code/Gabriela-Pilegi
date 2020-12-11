8b28824bc6c0bacb94ee56b803b2f3b5eba24509
unknown <email-que-voce-usa-no-github>
2020-12-11 12:13:06 -0300

    Exercício 3 de interpretação de código.

--- semana4/lista-de-exercicios/lista-de-exercicios.js
+++ semana4/lista-de-exercicios/lista-de-exercicios.js
@@ -9,3 +9,9 @@
 // Como faz: É criada a função investeDinheiro com os parâmetros tipoDeInvestimento e valor. Dentro dessa função, é definida uma variável chamada valorAposInvestimento, seguida de uma condicional de switch/case para poupança, renda fixa, CDB e ações, além de um alerta para uma informação que diverge de um desses 4 casos. Dentro de cada case, há a multiplicação do valor pelo tipo de investimento específico. Fora do switch/case, mas ainda dentro da função, é retornada a variável valorAposInvestimento, para posterior impressão no console. Fora da função, é retornado o nome da função por meio das variáveis novoMontante e segundoMontante, que têm, respectivamente, o valor de 1.1 (correspondente ao case ações) multiplicado por 150 (correspondente ao parâmetro valor), e Tipo de investimento incorreto, uma vez que não há case para Tesouro Direto. Os resultados são impressos no console por meio do console.log.
 // Valor impresso no console: 165 para a variável novoMontante e Tipo de investimento informado incorreto para o segundoMontante.
 
+// Exercício 3
+// O que faz: Programa para exibir a quantidade total de números do array inicial, assim como quantidades e números pares e ímpares em arrays específicas, por meio de loop por for of. 
+// Como faz: Há um array na variável numeros e dois arrays vazios nas variáveis array1 e array2. Na execução do programa, a variável numero é atribuída ao array numeros, representando cada um dos elementos contidos nele. Depois, há uma condicional que diz que se o resto da divisão por 2 do numero do array inicial for 0, ou seja, se for número par, este número será exibido no array1. Caso contrário, o número fará parte do array2, destinado a números ímpares. Após concluir o programa, são impressos no console a quantidade total de números do array inicial e as quantidades de números pares e ímpares.
+// Valor impresso no console: Quantidade total de números: 14; 6; 8
+//                                
+
