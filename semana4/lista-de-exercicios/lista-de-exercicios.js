f4c21d4609549bcc378f59c89588dd9d67a0d8d6
unknown <email-que-voce-usa-no-github>
2020-12-13 14:34:20 -0300

    Exercício 5 de Funções de array

--- semana4/lista-de-exercicios/lista-de-exercicios.js
+++ semana4/lista-de-exercicios/lista-de-exercicios.js
@@ -250,7 +250,6 @@
 // console.log(naoPodeEntrar)
 
 // Exercício 4
-
 // const consultas = [
 // 	{ nome: "João", genero: "masculino", cancelada: true, dataDaConsulta: "01/10/2019" },
 // 	{ nome: "Pedro", genero: "masculino", cancelada: false, dataDaConsulta: "02/10/2019" },
@@ -279,3 +278,22 @@
 // }
 
 // console.log(mensagensPacientes(consultas))
+
+// Exercício 5
+// const contas = [
+// 	{ cliente: "João", saldoTotal: 1000, compras: [100, 200, 300] },
+// 	{ cliente: "Paula", saldoTotal: 7500, compras: [200, 1040] },
+// 	{ cliente: "Pedro", saldoTotal: 10000, compras: [5140, 6100, 100, 2000] },
+// 	{ cliente: "Luciano", saldoTotal: 100, compras: [100, 200, 1700] },
+// 	{ cliente: "Artur", saldoTotal: 1800, compras: [200, 300] },
+// 	{ cliente: "Soter", saldoTotal: 1200, compras: [] }
+// ]
+
+// contas.forEach((atualizaSaldo) => {
+
+//     for (let i = 0; i < atualizaSaldo.compras.length; i++) {
+//         atualizaSaldo.saldoTotal -= atualizaSaldo.compras[i]
+//     } 
+//  })
+
+//  console.log(contas)
