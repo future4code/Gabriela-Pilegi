5a4d51cabba8f781dbaa11fafb79b67008eeacf7
unknown <email-que-voce-usa-no-github>
2021-01-12 18:49:09 -0300

    Alterações para deletar usuários - não finalizado

--- semana7-2/labenusers/src/components/ListaUsuarios.js
+++ semana7-2/labenusers/src/components/ListaUsuarios.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import axios from 'axios'
-import userEvent from '@testing-library/user-event'
+
 
 class ListaUsuarios extends React.Component{
     state = {
@@ -12,32 +12,51 @@ class ListaUsuarios extends React.Component{
     }
 
 
-    listarUsuarios = () =>
-    axios.get('https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users', {
-        headers: {
-            Authorization: 'gabriela-pilegi-epps'
-        }
-    }).then((response) => {
-        this.setState({ usuarios: response.data })
-    }).catch((error) => {
-        alert("Erro.")
-        console.log(error.message)
-    })
+    listarUsuarios = () =>{
+        axios.get('https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users', {
+            headers: {
+                Authorization: 'gabriela-pilegi-epps'
+            }
+        }).then((response) => {
+            this.setState({ usuarios: response.data })
+        }).catch((error) => {
+            alert("Erro.")
+            console.log(error.message)
+        }  )
+    }
+
+    deletarUsuario = (userId) => {
+        axios.delete(`https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/${userId}`, {
+            headers: {
+                Authorization: 'gabriela-pilegi-epps'
+            }
+        })
+          .then(() => {
+            alert("Usuário deletado com sucesso!")
+            this.listarUsuarios();
+          })
+          .catch((error) => {
+            alert("Erro ao deletar usuário. ")
+            console.log(error.message)
+          })
+      }
 
     render() {
         const exibirListaUsuarios = this.state.usuarios.map((usuario) => {
             return(
-                <p key={usuario.id}>
+                <div>
                     {usuario.name}
-                </p>
+                    <button onClick={() => this.deletarUsuario(usuario)}>X</button>
+                </div>
             )
         })
         return (
             <div>
                 <h1>Lista de usuários cadastrados</h1>
-                {exibirListaUsuarios}
+                <p>{exibirListaUsuarios}</p>
             </div>
         )
     }
 }
+
 export default ListaUsuarios;
