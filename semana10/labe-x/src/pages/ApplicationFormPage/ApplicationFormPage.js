ee8c0ad84087b855e2ae8dd6b54b09b1cecfb1f3
unknown <email-que-voce-usa-no-github>
2021-02-03 17:24:40 -0300

    Exercício em andamento

--- semana10/labe-x/src/pages/ApplicationFormPage/ApplicationFormPage.js
+++ semana10/labe-x/src/pages/ApplicationFormPage/ApplicationFormPage.js
@@ -1,12 +1,111 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
+
+const ApplicationForm = styled.div`
+    display: flex;
+    flex-direction: column;
+`
 
 
 const ApplicationFormPage = () => {
-    
+
+    const [form, setForm] = useState({
+        name:"",
+        age: "",
+        applicationText: "",
+        profession: "",
+        country: ""
+    })
+
+    const [trips, setTrips] = useState(
+        []
+    )
+
+    const handleInputChange = (event) => {
+        const { name, value } = event.target
+        setForm({...form, [name]: value })
+    }
+
+    useEffect(() => {
+        axios.get(
+            "https://us-central1-labenu-apis.cloudfunctions.net/labeX/gabriela-pilegi-epps/trips"
+            )
+            .then((response) => {
+                setTrips(response.data.trips)
+
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }, [])
+
+    const getTrips = trips.map((trip) => {
+        return (
+            <option value={trip.id} key={trip.id}>
+                <p>{trip.name} - {trip.planet}</p>
+                <p>{trip.description}</p>
+            </option>
+        )
+    })
     return (
-        <div>
+        <ApplicationForm>
             <h1>Application Form Page</h1>
-        </div>
+            <form>
+                <label>Nome: </label>
+                <input 
+                    name="name"
+                    value={form.name}
+                    onChange={handleInputChange}
+                    type="text"
+                    placeholder="Digite seu nome"
+                />
+                <label>Idade: </label>
+                <input 
+                    name="age"
+                    value={form.age}
+                    onChange={handleInputChange}
+                    type="number"
+                    placeholder="Digite sua idade"
+                />
+                <label>Profissão: </label>
+                <input 
+                    name="profession"
+                    value={form.profession}
+                    onChange={handleInputChange}
+                    type="text"
+                    placeholder="Digite sua profissão"
+                />
+                <label>País </label>
+                <input 
+                    name="country"
+                    value={form.country}
+                    onChange={handleInputChange}
+                    type="text"
+                    placeholder="Digite seu país"
+                />
+
+                <label>Destino: </label>
+                <select 
+                    name="tripId"
+                    value={form.tripId}
+                    onChange={handleInputChange}
+                >
+                <option value="">Escolha um destino</option>
+                {getTrips}
+                </select>
+                <label>Mensagem: </label>
+                <input 
+                    name="applicationTExt"
+                    value={form.applicationText}
+                    onChange={handleInputChange}
+                    type="text"
+                    placeholder="Por que você quer ir?"
+                />
+
+                <button>Inscrever-me</button>
+            </form>
+        </ApplicationForm>
     )
 }
 
--- semana10/labe-x/src/pages/TripDetailsPage/TripDetailsPage.js
+++ semana10/labe-x/src/pages/TripDetailsPage/TripDetailsPage.js
@@ -1,11 +1,45 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { useHistory } from 'react-router-dom'
 
 
 const TripDetailsPage = () => {
+
+    const [trip, setTrip] = useState({})
+    const history= useHistory()
+
+    useEffect(() => {
+        const token = window.localStorage.getItem("token")
+        if (token === null) {
+            history.push("/login")
+        }
+    }, [])
     
+   
+   const getTripDetail = () => {
+        axios.get(
+            `https://us-central1-labenu-apis.cloudfunctions.net/labeX/gabriela-pilegi-epps/trip/${trip.id}`,
+            {
+            headers: {
+                auth: localStorage.getItem("token")
+            }
+        })
+        .then((response) => {
+            setTrip(response.data.trip)
+        })
+        .catch((error) => {
+            console.log(error)
+        })      
+    }
+
+ 
+    
+
     return (
         <div>
             <h1>Trip Details Page</h1>
+            <h2>{trip.name}</h2>
+            <p>{trip.description}</p>
         </div>
     )
 }
